a914498a03c945da0c98d67653a28faed81875a5
Amarildo junior <amarildosousaxp02@gmail.com>
2023-10-25 23:55:43 -0300

    Project clean

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/amarildoxp" target="_blank" rel="noreferrer">Amarildo Junior</a></p>
+    <p className="text-light"><small><strong>Projeto Vendas</strong><br/>
+      Projeto de Vendas desenvolvido: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+
+  );
+};
+
+export default Footer;
